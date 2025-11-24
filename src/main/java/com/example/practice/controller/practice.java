1cd3bb0b5b5d6e6045af7cd81c88c04e8c87a4ee
Somarouthu <durga-sri-ranganadhagopi.somarouthu@capgemini.com>
2025-11-24 18:06:50 +0530

    comitted

--- src/main/java/com/example/practice/controller/practice.java
+++ src/main/java/com/example/practice/controller/practice.java
@@ -3,10 +3,17 @@ package com.example.practice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class practice {
     @GetMapping("/user")
-    public String userName(){
-        return  "gopi";
+    public String userName() {
+        return "gopi";
+    }
+    @GetMapping("/list")
+    public List<Integer> list() {
+        return new ArrayList<>(List.of(1, 2, 3, 4));
     }
 }
